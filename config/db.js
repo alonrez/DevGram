2d1fe0640bda61851bb85226885209ac29a177fc
alonrez <54244917+alonrez@users.noreply.github.com>
2021-01-22 20:56:52 +0200

    Profile API Routes

--- config/db.js
+++ config/db.js
@@ -1,17 +1,18 @@
 const mongoose = require('mongoose')
 const config = require('config')
 const db = config.get('mongoURI')
+mongoose.set('useFindAndModify', false)
 
 const connectDB = async () => {
     try {
-       await mongoose.connect(db,  {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true
-       })
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        })
 
-       console.log('MongoDB Connected...')
-    } catch(err) {
+        console.log('MongoDB Connected...')
+    } catch (err) {
         console.log(err.message)
         // exit process with failure
         process.exit(1)
